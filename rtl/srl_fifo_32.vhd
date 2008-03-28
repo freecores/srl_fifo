db5c950badf732ca51aa150e6a19533613dcff84
amulcock <amulcock@e5c1936c-7ba2-4726-ace4-4b9dcc42c23b>
2008-03-28 20:37:58 +0000

    Xilinx tools 10.1 adn above can infer SRL's better than before, can now infer cascaded srl's.

--- rtl/srl_fifo_32.vhd
+++ rtl/srl_fifo_32.vhd
@@ -1,51 +1,51 @@
 ----------------------------------------------------------------------------
-----												                    ----
-----												                    ----
----- This file is part of the srl_fifo project						    ----
----- http://www.opencores.org/cores/srl_fifo					        ----
-----												                    ----
----- Description										                ----
+----                                                                    ----
+----                                                                    ----
+---- This file is part of the srl_fifo project                          ----
+---- http://www.opencores.org/cores/srl_fifo                            ----
+----                                                                    ----
+---- Description                                                        ----
 ---- Implementation of srl_fifo IP core according to                    ----
----- srl_fifo IP core specification document.			                ----
-----												                    ----
----- To Do:											                    ----
-----	NA											                    ----
-----												                    ----
----- Author(s):										                    ----
-----   Andrew Mulcock, amulcock@opencores.org			                ----
-----												                    ----
+---- srl_fifo IP core specification document.                           ----
+----                                                                    ----
+---- To Do:                                                             ----
+----	NA                                                                ----
+----                                                                    ----
+---- Author(s):                                                         ----
+----   Andrew Mulcock, amulcock@opencores.org                           ----
+----                                                                    ----
 ----------------------------------------------------------------------------
-----												                    ----
----- Copyright (C) 2008 Authors and OPENCORES.ORG				        ----
-----												                    ----
----- This source file may be used and distributed without				----
----- restriction provided that this copyright statement is not			----
----- removed from the file and that any derivative work contains		----
----- the original copyright notice and the associated disclaimer.		----
-----												                    ----
----- This source file is free software; you can redistribute it			----
----- and/or modify it under the terms of the GNU Lesser General		    ----
----- Public License as published by the Free Software Foundation;		----
----- either version 2.1 of the License, or (at your option) any			----
----- later version.										                ----
-----												                    ----
----- This source is distributed in the hope that it will be				----
----- useful, but WITHOUT ANY WARRANTY; without even the implied	        ----
----- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR	        ----
----- PURPOSE. See the GNU Lesser General Public License for more		----
----- details.											                ----
-----												                    ----
----- You should have received a copy of the GNU Lesser General		    ----
----- Public License along with this source; if not, download it			----
----- from http://www.opencores.org/lgpl.shtml					        ----
-----												                    ----
+----                                                                    ----
+---- Copyright (C) 2008 Authors and OPENCORES.ORG                       ----
+----                                                                    ----
+---- This source file may be used and distributed without               ----
+---- restriction provided that this copyright statement is not          ----
+---- removed from the file and that any derivative work contains        ----
+---- the original copyright notice and the associated disclaimer.       ----
+----                                                                    ----
+---- This source file is free software; you can redistribute it         ----
+---- and/or modify it under the terms of the GNU Lesser General         ----
+---- Public License as published by the Free Software Foundation;       ----
+---- either version 2.1 of the License, or (at your option) any         ----
+---- later version.                                                     ----
+----                                                                    ----
+---- This source is distributed in the hope that it will be             ----
+---- useful, but WITHOUT ANY WARRANTY; without even the implied         ----
+---- warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR            ----
+---- PURPOSE. See the GNU Lesser General Public License for more        ----
+---- details.                                                           ----
+----                                                                    ----
+---- You should have received a copy of the GNU Lesser General          ----
+---- Public License along with this source; if not, download it         ----
+---- from http://www.opencores.org/lgpl.shtml                           ----
+----                                                                    ----
 ----------------------------------------------------------------------------
---												                        ----
--- CVS Revision History									                ----
---												                        ----
--- $Log: not supported by cvs2svn $											                    ----
---												                        ----
---
+----                                                                    ----
+-- CVS Revision History                                                 ----
+----                                                                    ----
+-- $Log: not supported by cvs2svn $                                                                ----
+----                                                                    ----
+----                                                                    ----
 -- quick description
 --
 --  Based upon the using a shift register as a fifo which has been 
@@ -57,10 +57,16 @@
 --    tecXeclusive article, which as at early 2008, Xilinx have
 --     removed.
 --
--- This version is for 'later' devices that have an inherent shift
--- register of 32 bits.
 --
-
+-- using Xilinx ISE 10.1 and later, the tools are getting real clever.
+--   In previous version of ISE, SRL inferance was not this clever.
+--     now if one infers a 32 bit srl in a device that has inherantly 16 
+--      bit srls, then an srl and a series of registers was created.
+--   In 10,1 and later, if you infer a 32 bit srl for a device with
+--    16 bit srls in, then you end up with the cascaded srls as expected.
+--
+--    Well done Xilinx..
+--
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.NUMERIC_STD.all;
